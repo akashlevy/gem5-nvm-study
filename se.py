c8f0943f263a8c801a061f7547d7a3572325c2cc
Akash Levy <akashlevy@gmail.com>
2023-08-27 15:48:01 -0700

    Fix se.py

--- se.py
+++ se.py
@@ -68,62 +68,62 @@ from nvm import *
 
 
 def get_processes(args):
-    """Interprets provided args and returns a list of processes"""
+  """Interprets provided args and returns a list of processes"""
 
-    multiprocesses = []
-    inputs = []
-    outputs = []
-    errouts = []
-    pargs = []
+  multiprocesses = []
+  inputs = []
+  outputs = []
+  errouts = []
+  pargs = []
 
-    workloads = args.cmd.split(";")
-    if args.input != "":
-        inputs = args.input.split(";")
-    if args.output != "":
-        outputs = args.output.split(";")
-    if args.errout != "":
-        errouts = args.errout.split(";")
-    if args.options != "":
-        pargs = args.options.split(";")
+  workloads = args.cmd.split(";")
+  if args.input != "":
+    inputs = args.input.split(";")
+  if args.output != "":
+    outputs = args.output.split(";")
+  if args.errout != "":
+    errouts = args.errout.split(";")
+  if args.options != "":
+    pargs = args.options.split(";")
 
-    idx = 0
-    for wrkld in workloads:
-        process = Process(pid=100 + idx)
-        process.executable = wrkld
-        process.cwd = os.getcwd()
-        process.gid = os.getgid()
+  idx = 0
+  for wrkld in workloads:
+    process = Process(pid=100 + idx)
+    process.executable = wrkld
+    process.cwd = os.getcwd()
+    process.gid = os.getgid()
 
-        if args.env:
-            with open(args.env, "r") as f:
-                process.env = [line.rstrip() for line in f]
+    if args.env:
+      with open(args.env, "r") as f:
+        process.env = [line.rstrip() for line in f]
 
-        if len(pargs) > idx:
-            process.cmd = [wrkld] + pargs[idx].split()
-        else:
-            process.cmd = [wrkld]
-
-        if len(inputs) > idx:
-            process.input = inputs[idx]
-        if len(outputs) > idx:
-            process.output = outputs[idx]
-        if len(errouts) > idx:
-            process.errout = errouts[idx]
-
-        multiprocesses.append(process)
-        idx += 1
-
-    if args.smt:
-        assert args.cpu_type == "DerivO3CPU"
-        return multiprocesses, idx
+    if len(pargs) > idx:
+      process.cmd = [wrkld] + pargs[idx].split()
     else:
-        return multiprocesses, 1
+      process.cmd = [wrkld]
+
+    if len(inputs) > idx:
+      process.input = inputs[idx]
+    if len(outputs) > idx:
+      process.output = outputs[idx]
+    if len(errouts) > idx:
+      process.errout = errouts[idx]
+
+    multiprocesses.append(process)
+    idx += 1
+
+  if args.smt:
+    assert args.cpu_type == "DerivO3CPU"
+    return multiprocesses, idx
+  else:
+    return multiprocesses, 1
 
 parser = argparse.ArgumentParser()
 Options.addCommonOptions(parser)
 Options.addSEOptions(parser)
 
 if "--ruby" in sys.argv:
-    Ruby.define_options(parser)
+  Ruby.define_options(parser)
 
 args = parser.parse_args()
 
@@ -131,37 +131,37 @@ multiprocesses = []
 numThreads = 1
 
 if args.bench:
-    apps = args.bench.split("-")
-    if len(apps) != args.num_cpus:
-        print("number of benchmarks not equal to set num_cpus!")
-        sys.exit(1)
-
-    for app in apps:
-        try:
-            if get_runtime_isa() == ISA.ARM:
-                exec(
-                    "workload = %s('arm_%s', 'linux', '%s')"
-                    % (app, args.arm_iset, args.spec_input)
-                )
-            else:
-                # TARGET_ISA has been removed, but this is missing a ], so it
-                # has incorrect syntax and wasn't being used anyway.
-                exec(
-                    "workload = %s(buildEnv['TARGET_ISA', 'linux', '%s')"
-                    % (app, args.spec_input)
-                )
-            multiprocesses.append(workload.makeProcess())
-        except:
-            print(
-                f"Unable to find workload for {get_runtime_isa().name()}: {app}",
-                file=sys.stderr,
-            )
-            sys.exit(1)
-elif args.cmd:
-    multiprocesses, numThreads = get_processes(args)
-else:
-    print("No workload specified. Exiting!\n", file=sys.stderr)
+  apps = args.bench.split("-")
+  if len(apps) != args.num_cpus:
+    print("number of benchmarks not equal to set num_cpus!")
     sys.exit(1)
+
+  for app in apps:
+    try:
+      if get_runtime_isa() == ISA.ARM:
+        exec(
+          "workload = %s('arm_%s', 'linux', '%s')"
+          % (app, args.arm_iset, args.spec_input)
+        )
+      else:
+        # TARGET_ISA has been removed, but this is missing a ], so it
+        # has incorrect syntax and wasn't being used anyway.
+        exec(
+          "workload = %s(buildEnv['TARGET_ISA', 'linux', '%s')"
+          % (app, args.spec_input)
+        )
+      multiprocesses.append(workload.makeProcess())
+    except:
+      print(
+        f"Unable to find workload for {get_runtime_isa().name()}: {app}",
+        file=sys.stderr,
+      )
+      sys.exit(1)
+elif args.cmd:
+  multiprocesses, numThreads = get_processes(args)
+else:
+  print("No workload specified. Exiting!\n", file=sys.stderr)
+  sys.exit(1)
 
 
 (CPUClass, test_mem_mode, FutureClass) = Simulation.setCPUClass(args)
@@ -169,26 +169,26 @@ CPUClass.numThreads = numThreads
 
 # Check -- do not allow SMT with multiple CPUs
 if args.smt and args.num_cpus > 1:
-    fatal("You cannot use SMT with multiple CPUs!")
+  fatal("You cannot use SMT with multiple CPUs!")
 
 np = args.num_cpus
 mp0_path = multiprocesses[0].executable
 system = System(
-    cpu=[CPUClass(cpu_id=i) for i in range(np)],
-    mem_mode=test_mem_mode,
-    mem_ranges=[AddrRange(args.mem_size)],
-    cache_line_size=args.cacheline_size,
+  cpu=[CPUClass(cpu_id=i) for i in range(np)],
+  mem_mode=test_mem_mode,
+  mem_ranges=[AddrRange(args.mem_size)],
+  cache_line_size=args.cacheline_size,
 )
 
 if numThreads > 1:
-    system.multi_thread = True
+  system.multi_thread = True
 
 # Create a top-level voltage domain
 system.voltage_domain = VoltageDomain(voltage=args.sys_voltage)
 
 # Create a source clock for the system and set the clock period
 system.clk_domain = SrcClockDomain(
-    clock=args.sys_clock, voltage_domain=system.voltage_domain
+  clock=args.sys_clock, voltage_domain=system.voltage_domain
 )
 
 # Create a CPU voltage domain
@@ -196,91 +196,91 @@ system.cpu_voltage_domain = VoltageDomain()
 
 # Create a separate clock domain for the CPUs
 system.cpu_clk_domain = SrcClockDomain(
-    clock=args.cpu_clock, voltage_domain=system.cpu_voltage_domain
+  clock=args.cpu_clock, voltage_domain=system.cpu_voltage_domain
 )
 
 # If elastic tracing is enabled, then configure the cpu and attach the elastic
 # trace probe
 if args.elastic_trace_en:
-    CpuConfig.config_etrace(CPUClass, system.cpu, args)
+  CpuConfig.config_etrace(CPUClass, system.cpu, args)
 
 # All cpus belong to a common cpu_clk_domain, therefore running at a common
 # frequency.
 for cpu in system.cpu:
-    cpu.clk_domain = system.cpu_clk_domain
+  cpu.clk_domain = system.cpu_clk_domain
 
 if ObjectList.is_kvm_cpu(CPUClass) or ObjectList.is_kvm_cpu(FutureClass):
-    if buildEnv["USE_X86_ISA"]:
-        system.kvm_vm = KvmVM()
-        system.m5ops_base = 0xFFFF0000
-        for process in multiprocesses:
-            process.useArchPT = True
-            process.kvmInSE = True
-    else:
-        fatal("KvmCPU can only be used in SE mode with x86")
+  if buildEnv["USE_X86_ISA"]:
+    system.kvm_vm = KvmVM()
+    system.m5ops_base = 0xFFFF0000
+    for process in multiprocesses:
+      process.useArchPT = True
+      process.kvmInSE = True
+  else:
+    fatal("KvmCPU can only be used in SE mode with x86")
 
 # Sanity check
 if args.simpoint_profile:
-    if not ObjectList.is_noncaching_cpu(CPUClass):
-        fatal("SimPoint/BPProbe should be done with an atomic cpu")
-    if np > 1:
-        fatal("SimPoint generation not supported with more than one CPUs")
+  if not ObjectList.is_noncaching_cpu(CPUClass):
+    fatal("SimPoint/BPProbe should be done with an atomic cpu")
+  if np > 1:
+    fatal("SimPoint generation not supported with more than one CPUs")
 
 for i in range(np):
-    if args.smt:
-        system.cpu[i].workload = multiprocesses
-    elif len(multiprocesses) == 1:
-        system.cpu[i].workload = multiprocesses[0]
-    else:
-        system.cpu[i].workload = multiprocesses[i]
+  if args.smt:
+    system.cpu[i].workload = multiprocesses
+  elif len(multiprocesses) == 1:
+    system.cpu[i].workload = multiprocesses[0]
+  else:
+    system.cpu[i].workload = multiprocesses[i]
 
-    if args.simpoint_profile:
-        system.cpu[i].addSimPointProbe(args.simpoint_interval)
+  if args.simpoint_profile:
+    system.cpu[i].addSimPointProbe(args.simpoint_interval)
 
-    if args.checker:
-        system.cpu[i].addCheckerCpu()
+  if args.checker:
+    system.cpu[i].addCheckerCpu()
 
-    if args.bp_type:
-        bpClass = ObjectList.bp_list.get(args.bp_type)
-        system.cpu[i].branchPred = bpClass()
+  if args.bp_type:
+    bpClass = ObjectList.bp_list.get(args.bp_type)
+    system.cpu[i].branchPred = bpClass()
 
-    if args.indirect_bp_type:
-        indirectBPClass = ObjectList.indirect_bp_list.get(
-            args.indirect_bp_type
-        )
-        system.cpu[i].branchPred.indirectBranchPred = indirectBPClass()
+  if args.indirect_bp_type:
+    indirectBPClass = ObjectList.indirect_bp_list.get(
+      args.indirect_bp_type
+    )
+    system.cpu[i].branchPred.indirectBranchPred = indirectBPClass()
 
-    system.cpu[i].createThreads()
+  system.cpu[i].createThreads()
 
 if args.ruby:
-    Ruby.create_system(args, False, system)
-    assert args.num_cpus == len(system.ruby._cpu_ports)
+  Ruby.create_system(args, False, system)
+  assert args.num_cpus == len(system.ruby._cpu_ports)
 
-    system.ruby.clk_domain = SrcClockDomain(
-        clock=args.ruby_clock, voltage_domain=system.voltage_domain
-    )
-    for i in range(np):
-        ruby_port = system.ruby._cpu_ports[i]
+  system.ruby.clk_domain = SrcClockDomain(
+    clock=args.ruby_clock, voltage_domain=system.voltage_domain
+  )
+  for i in range(np):
+    ruby_port = system.ruby._cpu_ports[i]
 
-        # Create the interrupt controller and connect its ports to Ruby
-        # Note that the interrupt controller is always present but only
-        # in x86 does it have message ports that need to be connected
-        system.cpu[i].createInterruptController()
+    # Create the interrupt controller and connect its ports to Ruby
+    # Note that the interrupt controller is always present but only
+    # in x86 does it have message ports that need to be connected
+    system.cpu[i].createInterruptController()
 
-        # Connect the cpu's cache ports to Ruby
-        ruby_port.connectCpuPorts(system.cpu[i])
+    # Connect the cpu's cache ports to Ruby
+    ruby_port.connectCpuPorts(system.cpu[i])
 else:
-    MemClass = eval(args.mem_type)
-    system.membus = SystemXBar()
-    system.system_port = system.membus.cpu_side_ports
-    CacheConfig.config_cache(args, system)
-    MemConfig.config_mem(args, system)
-    config_filesystem(system, args)
+  MemClass = Simulation.setMemClass(args)
+  system.membus = SystemXBar()
+  system.system_port = system.membus.cpu_side_ports
+  CacheConfig.config_cache(args, system)
+  MemConfig.config_mem(args, system)
+  config_filesystem(system, args)
 
 system.workload = SEWorkload.init_compatible(mp0_path)
 
 if args.wait_gdb:
-    system.workload.wait_for_remote_gdb = True
+  system.workload.wait_for_remote_gdb = True
 
 root = Root(full_system=False, system=system)
 Simulation.run(args, root, system, FutureClass)
